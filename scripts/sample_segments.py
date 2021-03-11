12b6e7a2b63c6cea9f8ec626abdef1cc58a88133
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-03-11 19:53:08 +0100

    random voc

--- scripts/sample_segments.py
+++ scripts/sample_segments.py
@@ -2,35 +2,46 @@ import pandas as pd
 
 from ChildProject.projects import ChildProject
 from ChildProject.annotations import AnnotationManager
-from ChildProject.pipelines.samplers import HighVolubilitySampler
+from ChildProject.pipelines.samplers import HighVolubilitySampler, RandomVocalizationSampler
 
 project = ChildProject('.')
 project.read()
 
 # get the top 20 1-minute regions per recording,
 # sorted by 'cvc' (https://github.com/LAAC-LSCP/ChildRecordsData/blob/12aaceaf6307b2f32d7a0faf5e0a36f8db1dcf15/ChildProject/pipelines/samplers.py#L305)
-sampler = HighVolubilitySampler(
+high_volubility_sampler = HighVolubilitySampler(
     project,
     annotation_set = 'its',
     windows_length = 60 * 1000,
-    windows_count = 20#,
-#    metric = 'cvc'
+    windows_count = 20,
+    metric = 'cvc'
 )
-sampler.sample()
+high_volubility_sampler.sample()
 
 am = AnnotationManager(project)
 am.read()
 annotations = am.annotations[am.annotations['set'] == 'its']
 
 # get ALL child vocalizations from these regions
-sample = []
-for recording, windows in sampler.segments.groupby('recording_filename'):
+high_volubility_samples = []
+for recording, windows in high_volubility_sampler.segments.groupby('recording_filename'):
     segments = am.get_segments(annotations[annotations['recording_filename'] == recording])
     segments = segments[['recording_filename', 'segment_onset', 'segment_offset', 'speaker_type']]
     segments = segments[segments['speaker_type'] == 'CHI']
     segments['chunk'] = (segments['segment_offset'] // 60000 + 1)
     windows['chunk'] = (windows['segment_offset'] // 60000 + 1)
+    segments = segments[segments['chunk'].isin(windows['chunk'].tolist())]
 
-    sample.append(segments[segments['chunk'].isin(windows['chunk'].tolist())])
+    high_volubility_samples.append(segments)
 
-print(pd.concat(sample))
+high_volubility_samples = pd.concat(high_volubility_samples)
+high_volubility_samples.to_csv('high.csv', index = False)
+
+random_sampler = RandomVocalizationSampler(
+    project,
+    annotation_set = 'its',
+    target_speaker_type = ['CHI'],
+    sample_size = 250
+)
+random_sampler.sample()
+random_sampler.segments[['recording_filename', 'segment_onset', 'segment_offset']].to_csv('random.csv', index = False)
