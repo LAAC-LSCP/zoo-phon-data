db63c5ce8f0b4976aca97c4e7f01ce98aa2f7de5
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-03-12 14:44:05 +0100

    change export destination

--- scripts/sample_segments.py
+++ scripts/sample_segments.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 from ChildProject.projects import ChildProject
@@ -14,7 +15,8 @@ high_volubility_sampler = HighVolubilitySampler(
     annotation_set = 'its',
     windows_length = 60 * 1000,
     windows_count = 20,
-    metric = 'cvc'
+    metric = 'cvc',
+    threads = 1
 )
 high_volubility_sampler.sample()
 
@@ -31,11 +33,12 @@ for recording, windows in high_volubility_sampler.segments.groupby('recording_fi
     segments['chunk'] = (segments['segment_offset'] // 60000 + 1)
     windows['chunk'] = (windows['segment_offset'] // 60000 + 1)
     segments = segments[segments['chunk'].isin(windows['chunk'].tolist())]
-
     high_volubility_samples.append(segments)
 
 high_volubility_samples = pd.concat(high_volubility_samples)
-high_volubility_samples.to_csv('high.csv', index = False)
+
+os.makedirs('samples/gold/high', exist_ok = True)
+high_volubility_samples.to_csv('samples/gold/high/samples.csv', index = False)
 
 random_sampler = RandomVocalizationSampler(
     project,
@@ -44,4 +47,5 @@ random_sampler = RandomVocalizationSampler(
     sample_size = 250
 )
 random_sampler.sample()
-random_sampler.segments[['recording_filename', 'segment_onset', 'segment_offset']].to_csv('random.csv', index = False)
+os.makedirs('samples/gold/random', exist_ok = True)
+random_sampler.segments[['recording_filename', 'segment_onset', 'segment_offset']].to_csv('samples/gold/random/samples.csv', index = False)
